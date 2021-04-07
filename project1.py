c7253f25633bede1d119b1fc2fab0397a8c15f12
sahana1318 <82105292+sahana1318@users.noreply.github.com>
2021-04-07 20:25:30 +0530

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,9 @@
+'''import math
+r=float(input('enter the radius of circle'))
+a=math.pi*(r**2)
+print('the area of the circle of radius',r,'is',a)'''
+
+filename=input('enter the filename')
+ext=filename.split('.')
+print(ext[-1])
+
---
+++ project2.py
@@ -0,0 +1,3 @@
+filename=input('enter the filename')
+ext=filename.split('.')
+print(ext[-1])
